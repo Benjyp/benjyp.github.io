584accdab7c74d9dd3e1bf17f194e4ca072e426d
Benjyp <benjymp@gmail.com>
2020-04-24 11:51:53 -0400

    Added color classification

--- roses.js
+++ roses.js
@@ -3,16 +3,53 @@ window.addEventListener('load', function () {
     document.getElementById("rose2").value = "11-11-10-10";
 });
 
-function cross(pair1, pair2) {
-    combinations = [];
-    for (var gene1 of pair1) {
-        for (var gene2 of pair2) {
-            var newPair = gene1+gene2;
-            var newPair = ((newPair == "01") ? '10' : newPair);
-            combinations.push(newPair);
+function cross(gene1, gene2) {
+    genes = [];
+    for (var allele1 of gene1) {
+        for (var allele2 of gene2) {
+            var newGene = allele1+allele2;
+            var newGene = ((newGene == "01") ? '10' : newGene);
+            genes.push(newGene);
         }
     }
-    return combinations;
+    return genes;
+}
+
+function getColor(genome) {
+    genes = genome.split('-');
+
+    if (genes[0] != '00') {
+        if (genes[0] == '11' && genes[1] == '11' && genes[2] == '00' && genes[3] == '00') {
+            return 'blue';
+        }
+        else if (genes[3] == '11') {
+            return 'pink';
+        }
+        else if (genes[3] == '00') {
+            if (genes[1] != '00') {
+                return 'orange';
+            }
+            else {
+                return 'black';
+            }
+        }
+        else {
+            return 'red';
+        }
+    }
+    else {
+        if (genes[1] != '00') {
+            return 'yellow';
+        }
+        else {
+            if (genes[2] != '00') {
+                return 'white';
+            }
+            else {
+                return 'purple';
+            }
+        }
+    }
 }
 
 function makeBBs() {
@@ -20,7 +57,6 @@ function makeBBs() {
     var rose2vals = document.getElementById("rose2").value.split('-');
     var rose1 = {r:rose1vals[0], y:rose1vals[1], w:rose1vals[2], s:rose1vals[3]};
     var rose2 = {r:rose2vals[0], y:rose2vals[1], w:rose2vals[2], s:rose2vals[3]};
-    //var rose2 = {r:"00", y:"00", w:"10", s:"00"};
     var roseBB = {r:cross(rose1.r, rose2.r), y:cross(rose1.y, rose2.y), w:cross(rose1.w, rose2.w), s:cross(rose1.s, rose2.s)};
     
     var BBs = [];
@@ -36,7 +72,7 @@ function makeBBs() {
         }
     }
     
-    results = {};
+    results = [];
     for (var BB of BBs) {
         if (BB in results) {
             results[BB] += 1;
@@ -45,11 +81,25 @@ function makeBBs() {
             results[BB] = 1;
         }
     }
-    
-    var output = "";
-    for (var [key, value] of Object.entries(results)) {
-        output += key+" : "+(value/BBs.length)*100+"%</br>";
+
+    for (var [genome, count] of Object.entries(results)) {
+        results[genome] = (count/BBs.length)*100;
     }
+
+    var sorted = Object.keys(results).map(function(key) {
+        return [key, results[key]];
+    });
+  
+    sorted.sort(function(first, second) {
+        return second[1] - first[1];
+    });
+
+    var output = "<table>";
+    for (var [genome, odds] of sorted) {
+        var color = getColor(genome);
+        output += '<tr><td class="genome">'+genome+' </td><td class="percent">: '+odds+"% ("+color+")</td></tr>";
+    }
+    output += "</table>";
     
     console.log(BBs);
     document.getElementById("output").innerHTML = output;
